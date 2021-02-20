bf0908e039f9aebd926bdc51af757bf44cd3ca18
shx27821 <62169872+shx27821@users.noreply.github.com>
2021-02-20 19:37:19 +0800

    Update github_commit_update.py

--- github_commit_update.py
+++ github_commit_update.py
@@ -2,9 +2,9 @@ import json, time, sys
 from requests import get, post
 
 token = str(sys.argv[1])
-main = json.loads(get("https://api.github.com/repos/Luck6-Project/Luck6-Docs/commits/main").content)
+main = json.loads(get("https://api.github.com/repos/Luck6-Project/Luck6-Controller/commits/main").content)
 text = "#update #docs #" + main['commit']['author']['name'].replace('_', '') + \
-       ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/Luck6-Project/Luck6-Docs/commit/' + \
+       ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/Luck6-Project/Luck6-Controller/commit/' + \
        main['sha'] + '): ' + main['commit']['message']
 push_content = {'chat_id': '-1001341765556', 'disable_web_page_preview': 'True' ,'parse_mode': 'markdown',
                 'text': text}
